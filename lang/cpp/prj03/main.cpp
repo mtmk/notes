8f193d12caeaeedfd78691915aa5802ec6865bf8
Ziya Suzen <ziya@suzen.net>
2022-02-18 17:14:13 +0000

    [lang/cpp] References for loop

--- lang/cpp/prj03/main.cpp
+++ lang/cpp/prj03/main.cpp
@@ -5,41 +5,61 @@ int main() {
 
 	// Array
 	{
-		std::cout << std::endl;
-		std::cout << "Array init" << std::endl;
+		std::cout << "Array init: ";
 		int arr[] = {1, 2, 3, 4};
 		for (auto i: arr) {
-			std::cout << i << std::endl;
+			std::cout << i << " ";
 		}
-	
 		std::cout << std::endl;
-		std::cout << "Array init 2 (uninitialized)" << std::endl;
+	
+		std::cout << "Array init (uninitialized): ";
 		int arr2[5];
 		for (auto i: arr2) {
-			std::cout << i << std::endl;
+			std::cout << i << " ";
 		}
+		std::cout << std::endl;
 	}
 
 	// Vector
 	{
-		std::cout << std::endl;
-		std::cout << "Vector init" << std::endl;
+		std::cout << "Vector init: ";
 		std::vector<int> vec  {1, 2, 3, 4};
 		for (auto i: vec) {
-			std::cout << i << std::endl;
+			std::cout << i << " ";
 		}
+		std::cout << std::endl;
 	}
 
 	// Pointers
 	{
-		std::cout << std::endl;
 		std::cout << "Pointers" << std::endl;
 		char cs[] = "Hi!";
+		std::cout << " arr: ";
 		for (auto c: cs) {
-			std::cout << c << std::endl;
+			std::cout << c << " ";
 		}
+		std::cout << std::endl;
 		char* p = &cs[1];
-		std::cout << "pointer to cs[1] = " << *p << std::endl;
+		std::cout << " pointer to cs[1] = " << *p << std::endl;
+	}
+
+	// References
+	{
+		std::cout << "References" << std::endl;
+		int arr[] = {1, 2, 3, 4, 5};
+		std::cout << " arr: ";
+		for (auto& i: arr) {
+			std::cout << i << " ";
+			i = i * i;
+		}
+		std::cout << std::endl;
+		std::cout << " sqr: ";
+		for (auto i: arr) {
+			std::cout << i << " ";
+		}
+		std::cout << std::endl;
+		
+
 	}
 
 }
