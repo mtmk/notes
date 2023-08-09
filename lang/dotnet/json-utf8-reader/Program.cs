f0016c9ce84c5c62ff52838378cc173e7b21a737
Ziya Suzen <ziya@suzen.net>
2023-08-09 15:21:28 +0100

    [lang/dotnet] JSON reader

---
+++ lang/dotnet/json-utf8-reader/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Text;
+using System.Text.Json;
+
+var json = """
+           [{
+               "name": "a",
+               "id": 1
+           }]
+           """;
+
+var options = new JsonReaderOptions
+{
+    AllowTrailingCommas = true,
+    CommentHandling = JsonCommentHandling.Skip
+};
+var reader = new Utf8JsonReader(Encoding.UTF8.GetBytes(json), options);
+
+while (reader.Read())
+{
+    Console.Write(reader.TokenType);
+
+    switch (reader.TokenType)
+    {
+        case JsonTokenType.PropertyName:
+        case JsonTokenType.String:
+        {
+            if (reader.ValueTextEquals(Encoding.UTF8.GetBytes("name")))
+            {
+                Console.WriteLine(">> found name");
+            }
+            string? text = reader.GetString();
+            Console.Write(" ");
+            Console.Write(text);
+            break;
+        }
+
+        case JsonTokenType.Number:
+        {
+            int intValue = reader.GetInt32();
+            Console.Write(" ");
+            Console.Write(intValue);
+            break;
+        }
+
+        // Other token types elided for brevity
+    }
+    Console.WriteLine();
+}
