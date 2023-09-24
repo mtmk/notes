fa5a960a4def0c9837cdbca4178dcf445bf895f6
Ziya Suzen <ziya@suzen.net>
2023-09-24 14:50:04 +0100

    [lang/cpp] tour of cpp ch11

---
+++ lang/cpp/tour_of_cpp/ch11.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+namespace tocpp_ch11 {
+	void run() {
+		cout << "CH11 Input and Output" << endl << endl;
+
+	}
+}
--- lang/cpp/tour_of_cpp/main.cpp
+++ lang/cpp/tour_of_cpp/main.cpp
@@ -2,11 +2,13 @@ namespace tocpp_ch1 { void run(); }
 namespace tocpp_ch2 { void run(); }
 namespace tocpp_ch3 { void run(); }
 namespace tocpp_ch10 { void run(); }
+namespace tocpp_ch11 { void run(); }
 
 int main()
 {
 	// tocpp_ch1::run();
 	// tocpp_ch2::run();
-	//tocpp_ch3::run();
-	tocpp_ch10::run();
+	// tocpp_ch3::run();
+	// tocpp_ch10::run();
+	tocpp_ch11::run();
 }
