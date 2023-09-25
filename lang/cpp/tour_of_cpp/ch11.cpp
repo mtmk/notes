5aa632fcce999b1d7c2763511253c8cbb263d128
Ziya Suzen <ziya@suzen.net>
2023-09-25 18:55:19 +0100

    [lang/cpp] tour of cpp ch11

--- lang/cpp/tour_of_cpp/ch11.cpp
+++ lang/cpp/tour_of_cpp/ch11.cpp
@@ -3,8 +3,24 @@
 using namespace std;
 
 namespace tocpp_ch11 {
+
+	struct Entity
+	{
+		string name;
+		int number;
+
+	};
+
+	ostream& operator<<(ostream& os, const Entity& e)
+	{
+		return os << "{name:" << e.name << ", number:" << e.number << "}";
+	}
+
 	void run() {
 		cout << "CH11 Input and Output" << endl << endl;
 
+		Entity e{ "blah", 42 };
+
+		cout << "e=" << e << endl;
 	}
 }
