45c56404b1f784f86ab215b15a2ac435565c051b
Ziya Suzen <ziya@suzen.net>
2024-03-31 12:15:46 +0100

    [lang/dotnet] roslyn

---
+++ lang/dotnet/roslyn-stuff/Class1.cs
@@ -0,0 +1,15 @@
+﻿namespace roslyn_stuff;
+
+public class Class1
+{
+    public void Method1()
+    {
+        Console.WriteLine("Hello from Class1.Method1");
+    }
+    
+    public void Method2()
+    {
+        Console.WriteLine("Hello from Class1.Method2");
+        Console.WriteLine("...twice!");
+    }
+}
---
+++ lang/dotnet/roslyn-stuff/Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+var filePath = "C:\\Users\\mtmk\\src\\notes\\lang\\dotnet\\roslyn-stuff\\Class1.cs"; // Replace with your actual file path
+var code = File.ReadAllText(filePath);
+var tree = CSharpSyntaxTree.ParseText(code);
+var root = tree.GetRoot();
+
+var walker = new MethodWalker();
+walker.Visit(root);
+
+class MethodWalker : CSharpSyntaxWalker
+{
+    public override void VisitMethodDeclaration(MethodDeclarationSyntax node)
+    {
+        var lineSpan = node.GetLocation().GetLineSpan();
+        var start = lineSpan.StartLinePosition.Line;
+        var end = lineSpan.EndLinePosition.Line;
+        var count = end - start;
+
+        Console.WriteLine($"Method found: {node.Identifier.ValueText}, number of lines: {count}");
+
+        base.VisitMethodDeclaration(node);
+    }
+}
